1d78035bde79f1532f8255e95ce241b7c944cb09
mikeybow <micbowman25@gmail.com>
2021-09-30 13:11:38 -0500

    easier to read

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ months_remaining = years_remaining * 12
 weeks_remaining = years_remaining * 52
 days_remaining = years_remaining * 365
 
-print(f"You have {days_remaining}, {weeks_remaining} weeks, and {months_remaining} months left.")
+total_time = (f"You have {days_remaining}, {weeks_remaining} weeks, and {months_remaining} months left.")
+
+print(total_time)
